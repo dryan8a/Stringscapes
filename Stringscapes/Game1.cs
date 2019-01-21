d9fa0321eaaf0c94163616b1174465cf65e23c21
Dylan Ochoa <Dylan.Ochoa@GMR.local>
2019-01-20 16:34:27 -0800

    Created the title screen and started on the Casual and Timed OPtions. Had a issue with the background of the buttons not drawing and managed to fix it but I only had enough time to fix it for the Casual button. Next time, implement the fix into the Timed button, make the buttons change color when hovered over, and customize the two screens

--- Stringscapes/Game1.cs
+++ Stringscapes/Game1.cs
@@ -13,6 +13,7 @@ namespace Stringscapes
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         MouseState mouse;
+        Rectangle mouseRect;
         Stringscape stringscape;
         Sprite reshuffleButton;
         MouseState previous;
@@ -26,7 +27,11 @@ namespace Stringscapes
         SpriteFont wordListFont;
         SpriteFont titleFont;
         Dictionary<int, string> words = new Dictionary<int, string>();
-        enum StartScreenState
+        Sprite casualOptionsButton;
+        Sprite timedOptionsButton;
+        Sprite backArrow;
+
+        enum ScreenState
         {
             TitleScreen,
             CasualOptions,
@@ -34,6 +39,7 @@ namespace Stringscapes
             Game,
             EndOfRoundOptions
         }
+        ScreenState GameState = ScreenState.TitleScreen;
 
         public Game1()
         {
@@ -67,6 +73,7 @@ namespace Stringscapes
             }
 
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
             baseCircleTexture = Content.Load<Texture2D>("nonBlurryCircleScaled");
             letterTexture = Content.Load<Texture2D>("LetterCircle");
             leftRightArrowTexture = Content.Load<Texture2D>("arrow");
@@ -75,6 +82,16 @@ namespace Stringscapes
             wordListFont = Content.Load<SpriteFont>("WordListFont");
             definitionFont = Content.Load<SpriteFont>("definitionFont");
             titleFont = Content.Load<SpriteFont>("titleFont");
+
+            var casualPixel = new Texture2D(GraphicsDevice, 1,1);
+            casualPixel.SetData(new[] { Color.White });            
+            casualOptionsButton = new Sprite(casualPixel, new Vector2(GraphicsDevice.Viewport.Width / 2 - (casualPixel.Width * wordListFont.MeasureString("- Timed").X / 2), 600), Color.TransparentBlack, GraphicsDevice) { Scale = wordListFont.MeasureString("- Timed") };
+            
+            var timedPixel = new Texture2D(GraphicsDevice, (int)wordListFont.MeasureString("- Timed").X, (int)wordListFont.MeasureString("- Timed").Y);
+            timedOptionsButton = new Sprite(timedPixel, new Vector2(GraphicsDevice.Viewport.Width / 2 - (timedPixel.Width / 2), 750), Color.TransparentBlack, GraphicsDevice);
+
+            backArrow = new Sprite(leftRightArrowTexture, new Vector2(10, 10), Color.Black, GraphicsDevice);
+
             string baseWord = "";
             while (baseWord == "")
             {
@@ -98,54 +115,119 @@ namespace Stringscapes
                 Exit();
 
             mouse = Mouse.GetState();
+            mouseRect = new Rectangle(mouse.Position, new Point(1, 1));
+
             //System.Diagnostics.Debug.WriteLine($"Mouse: ({mouse.X}, {mouse.Y})");
 
-
-            if (Math.Pow(reshuffleButton.Position.X + reshuffleButton.Radius - mouse.Position.X, 2) + Math.Pow(reshuffleButton.Position.Y + reshuffleButton.Radius - mouse.Position.Y, 2) <= Math.Pow(reshuffleButton.Radius, 2) && mouse.LeftButton == ButtonState.Pressed && previous.LeftButton != ButtonState.Pressed)
+            switch (GameState)
             {
-                stringscape.Reshuffle();
-            }
-
-            stringscape.Update(mouse);
-            if (stringscape.chosenWord.Length > 2)
-            {
-                bool containsWord = false;
-                for (int i = 0; i < stringscape.CorrectWords.Count; i++)
-                {
-                    if (stringscape.CorrectWords[i].word == stringscape.chosenWord)
+                case ScreenState.TitleScreen:
+                    if (mouseRect.Intersects(casualOptionsButton.Bounds))
                     {
-                        containsWord = true;
+                        if (mouse.LeftButton == ButtonState.Pressed)
+                        {
+                            GameState = ScreenState.CasualOptions;
+                        }
                     }
-                }
+                    else
+                    {
+                        casualOptionsButton.Color = Color.TransparentBlack;
+                    }
 
-                if (words.ContainsValue(stringscape.chosenWord) && !containsWord)
-                {
-                    stringscape.CorrectWords.Add(new CorrectWord(stringscape.chosenWord, wordListFont));
-                }
-                stringscape.chosenWord = "";
+                    if (mouseRect.Intersects(timedOptionsButton.Bounds))
+                    {
+                        if (mouse.LeftButton == ButtonState.Pressed)
+                        {
+                            GameState = ScreenState.TimedOptions;
+                        }
+                    }
+                    else
+                    {
+                        timedOptionsButton.Color = Color.TransparentBlack;
+                    }
+                    break;
 
+                case ScreenState.CasualOptions:
+                    if (mouse.LeftButton == ButtonState.Pressed && mouseRect.Intersects(backArrow.Bounds))
+                    {
+                        GameState = ScreenState.TitleScreen;
+                    }
+                    break;
+
+                case ScreenState.TimedOptions:
+                    if (mouse.LeftButton == ButtonState.Pressed && mouseRect.Intersects(backArrow.Bounds))
+                    {
+                        GameState = ScreenState.TitleScreen;
+                    }
+                    break;
+
+                case ScreenState.Game:
+                    if (Math.Pow(reshuffleButton.Position.X + reshuffleButton.Radius - mouse.Position.X, 2) + Math.Pow(reshuffleButton.Position.Y + reshuffleButton.Radius - mouse.Position.Y, 2) <= Math.Pow(reshuffleButton.Radius, 2) && mouse.LeftButton == ButtonState.Pressed && previous.LeftButton != ButtonState.Pressed)
+                    {
+                        stringscape.Reshuffle();
+                    }
+
+                    stringscape.Update(mouse);
+                    if (stringscape.chosenWord.Length > 2)
+                    {
+                        bool containsWord = false;
+                        for (int i = 0; i < stringscape.CorrectWords.Count; i++)
+                        {
+                            if (stringscape.CorrectWords[i].word == stringscape.chosenWord)
+                            {
+                                containsWord = true;
+                            }
+                        }
+
+                        if (words.ContainsValue(stringscape.chosenWord) && !containsWord)
+                        {
+                            stringscape.CorrectWords.Add(new CorrectWord(stringscape.chosenWord, wordListFont));
+                        }
+                        stringscape.chosenWord = "";
+
+                    }
+                    break;
+
+                case ScreenState.EndOfRoundOptions:
+                    break;
             }
-
             previous = mouse;
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
 
+            switch (GameState)
+            {
+                case ScreenState.TitleScreen:
+                    spriteBatch.DrawString(titleFont, "Stringscapes", new Vector2((GraphicsDevice.Viewport.Width / 2) - (titleFont.MeasureString("Stringscapes").X / 2), 50), Color.Black);
+                    casualOptionsButton.Draw(spriteBatch);
+                    spriteBatch.DrawString(wordListFont, "- Casual", casualOptionsButton.Position, Color.Black);
+                    timedOptionsButton.Draw(spriteBatch);
+                    spriteBatch.DrawString(wordListFont, "- Timed", timedOptionsButton.Position, Color.Black);                    
+                    break;
 
+                case ScreenState.CasualOptions:
+                    spriteBatch.DrawString(wordListFont, "Casual Options:", new Vector2((GraphicsDevice.Viewport.Width / 2) - (wordListFont.MeasureString("Casual Options:").X / 2), 50), Color.Black);
+                    backArrow.Draw(spriteBatch);
+                    break;
 
-            spriteBatch.DrawString(titleFont, "Stringscapes", new Vector2((GraphicsDevice.Viewport.Width / 2) - (titleFont.MeasureString("Stringscapes").X / 2), 50), Color.Black);
+                case ScreenState.TimedOptions:
+                    spriteBatch.DrawString(wordListFont, "Timed Options:", new Vector2((GraphicsDevice.Viewport.Width / 2) - (wordListFont.MeasureString("Timed Options:").X / 2), 50), Color.Black);
+                    backArrow.Draw(spriteBatch);
+                    break;
 
+                case ScreenState.Game:
+                    stringscape.Draw(spriteBatch);
+                    reshuffleButton.Draw(spriteBatch);
+                    break;
 
-
-            stringscape.Draw(spriteBatch);
-            reshuffleButton.Draw(spriteBatch);
-
-
+                case ScreenState.EndOfRoundOptions:
+                    break;
+            }
             spriteBatch.End();
             base.Draw(gameTime);
         }
